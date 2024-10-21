28b6e69868d6d0f220b73b0720cc84c5c819d162
Wertdrof <wertdrof@gmail.com>
2024-10-22 00:58:54 +0400

    initial commit

---
+++ NullableStructEnumIndexer/ListInt.cs
@@ -0,0 +1,92 @@
+﻿namespace NullableStructEnumIndexer
+{
+    internal class ListInt
+    {
+        private int[] array = new int[0];
+        public ListInt(int size)
+        {
+            array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", array);
+        }
+
+
+        public void Add(int value)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = value;
+        }
+        public void AddRange(params int[] nums)
+        {
+            int oldLen = array.Length;
+            Array.Resize(ref array, array.Length + nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                array[oldLen + i] = nums[i];
+            }
+        }
+        public bool Contains(int num)
+        {
+            bool found = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num)
+                {
+                    found = true;
+                    Console.WriteLine("NUmber found");
+                    break;
+                }
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Not found");
+            }
+            return found;
+        }
+        public void Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine($"Sum is {sum}");
+        }
+        public void Remove(int num)
+        {
+            int index = Array.IndexOf(array, num);
+            if (index == -1) return;
+            int[] newArray = new int[array.Length - 1];
+            int newIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index) continue;
+                newArray[newIndex++] = array[i];
+            }
+            array = newArray;
+        }
+        public void RemoveRange(params int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                Remove(num);
+            }
+        }
+
+    }
+}
+
---
+++ NullableStructEnumIndexer/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NullableStructEnumIndexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt arr = new ListInt(1);
+            arr[0] = 13;
+            arr.Add(1);
+            arr.AddRange(1, 3, 534, 465, 465, 47, 47, 47545, 74);
+            Console.WriteLine(arr);
+            arr.AddRange(1, 2, 3, 4);
+            Console.WriteLine(arr);
+            arr.RemoveRange(3, 4, 46);
+            arr.Sum();
+            Console.WriteLine(arr);
+
+
+
+
+        }
+    }
+}
